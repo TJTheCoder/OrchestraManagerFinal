4314d112b56b578c5bac5ddb2b4243f2fbb0f74f
TJTheCoder <tanujt2k5@gmail.com>
2023-05-17 08:51:00 -0500

    JFugueTest Complete!

---
+++ OrchestraManagerFinal/src/main/java/com/mycompany/orchestramanagerfinal/JFugueTest.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.orchestramanagerfinal;
+
+import org.jfugue.player.Player;
+
+/**
+ *
+ * @author tanuj
+ */
+public class JFugueTest {
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.play("C D E F G A B");
+    }
+}
